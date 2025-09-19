32a1b9a6cc9e20d3a256f37719086852794ce89c
Tibule12 <thulani.mtshwelo@capaciti.org.za>
2025-09-19 13:57:07 +0200

    Add basic authentication middleware to fix route handler error

---
+++ authMiddleware.js
@@ -0,0 +1,6 @@
+// Basic authentication middleware template
+module.exports = (req, res, next) => {
+	// TODO: Replace with real authentication logic
+	// For now, allow all requests to proceed
+	next();
+};
